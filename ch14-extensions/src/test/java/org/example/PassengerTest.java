18c3094f636d1579b9ed6ed293f008faaf98d827
deividasbardzenis <deividasbarzdenis@yahoo.com>
2022-06-19 11:25:24 +0300

    Implemented methods to test insert, update and delete passenger.

--- ch14-extensions/src/test/java/org/example/PassengerTest.java
+++ ch14-extensions/src/test/java/org/example/PassengerTest.java
@@ -2,6 +2,8 @@ package org.example;
 
 import org.example.extensions.DatabaseOperationsExtension;
 import org.example.extensions.ExecutionContextExtension;
+import org.example.jdbc.PassengerDao;
+import org.example.jdbc.PassengerExistsException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -12,11 +14,47 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith({ExecutionContextExtension.class, DatabaseOperationsExtension.class})
 class PassengerTest {
 
+    private PassengerDao passengerDao;
+
+    public PassengerTest(PassengerDao passengerDao) {
+        this.passengerDao = passengerDao;
+    }
+
     @Test
     void testPassenger() throws IOException {
         Passenger passenger = new Passenger("123-456-789", "John Smith");
         assertEquals("Passenger John Smith with identifier: 123-456-789",
                 passenger.toString());
+    }
+
+    @Test
+    void testInsertPassenger() throws PassengerExistsException {
+        Passenger passenger = new Passenger("123-456-789", "John Smith");
+
+        passengerDao.insert(passenger);
+
+        assertEquals("John Smith", passengerDao.getById("123-456-789").getName());
+    }
+
+    @Test
+    void testUpdatePassenger() throws PassengerExistsException {
+        Passenger passenger = new Passenger("123-456-789", "John Smith");
+
+        passengerDao.insert(passenger);
+        passengerDao.update("123-456-789", "Micheal Smith");
+
+        assertEquals("Micheal Smith", passengerDao.getById("123-456-789").getName());
+    }
+
+    @Test
+    void testDeletePassenger() throws PassengerExistsException {
+        Passenger passenger = new Passenger("123-456-789", "John Smith");
+
+        passengerDao.insert(passenger);
+        passengerDao.delete(passenger);
+
+        assertNotNull(passengerDao.getById("123-456-789"));
+
     }
 
 }
